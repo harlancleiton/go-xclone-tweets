7a1294541cecbdc5b53340adef4bbcfb38f1c74d
Harlan Cleiton <harlancleiton@gmail.com>
2024-02-23 05:56:45 -0300

    feat(cmd/server): start grpc server and create tweet

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/harlancleiton/go-tweets/internal/dto"
+	"github.com/harlancleiton/go-tweets/internal/usecases"
+	"github.com/harlancleiton/go-tweets/pkg/pb"
+	"google.golang.org/grpc"
+)
+
+type TweetServiceServerAdapter struct {
+	createTweet *usecases.CreateTweet
+	pb.UnimplementedTweetServiceServer
+}
+
+func (adapter *TweetServiceServerAdapter) Create(ctx context.Context, request *pb.CreateTweetRequest) (*pb.TweetResponse, error) {
+	input := dto.CreateTweetInput{
+		Text: request.Text,
+	}
+	tweet, err := adapter.createTweet.Execute(ctx, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.TweetResponse{
+		Id:        tweet.ID,
+		Text:      tweet.Text,
+		CreatedAt: tweet.CreatedAt.String(),
+	}, nil
+}
+
+func NewTweetServiceServerAdapter(createTweet *usecases.CreateTweet) *TweetServiceServerAdapter {
+	return &TweetServiceServerAdapter{
+		createTweet: createTweet,
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	options := []grpc.ServerOption{}
+	server := grpc.NewServer(options...)
+
+	pb.RegisterTweetServiceServer(server, NewTweetServiceServerAdapter(usecases.NewCreateTweet()))
+
+	listener, err := net.Listen("tcp", "localhost:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
